3ff12267fc0fdbe8bffaaca1b6c0c26061cf5b78
sakaguchi <ee177026@meiji.ac.jp>
2019-08-05 21:34:18 +0900

    fix eslint error

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
+'use strict';
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const token = '';
 const http = require('http');
 
 const server = http.createServer((req,res) => {
-  res.writeHead(200,{"Content-Type": "text/plain"});
-  res.end("discord bot activated");
+    res.writeHead(200,{'Content-Type': 'text/plain'});
+    res.end('discord bot activated');
 });
 const port = process.env.PORT || 8080;
 server.listen(port);
@@ -13,109 +14,109 @@ server.listen(port);
 const commandMap = new Map();
 
 function makeMemberList(channel){
-  const list = [];
-  for(member of channel.members){
-    list.push(member[1].user);
-  }
-  return list;
+    const list = [];
+    for(let member of channel.members){
+        list.push(member[1].user);
+    }
+    return list;
 }
 
 function findVoicechannel(message){
-  for(channel of message.guild.channels){
-    if(channel[1].type === 'voice'){
-      for(member of channel[1].members){
-        if(member[1].id === message.author.id){
-          return channel[1];
+    for(let channel of message.guild.channels){
+        if(channel[1].type === 'voice'){
+            for(let member of channel[1].members){
+                if(member[1].id === message.author.id){
+                    return channel[1];
+                }
+            }
         }
-      }
     }
-  }
 }
 
 function randomSort(members){
-  members.sort(() => Math.random() - Math.random());
+    members.sort(() => Math.random() - Math.random());
 }
 
 function teamSuffle(message){
-  const numOfTeam = message.content.split(" ")[1];
+    const numOfTeam = message.content.split(' ')[1];
 
-  if(numOfTeam === undefined){
-    message.reply(
-      `plz input number of team
-      eg) !team 2`);
-    return;
-  }
-
-  const channel = findVoicechannel(message);
-
-  if(channel === undefined){
-    message.reply("plz join voicechannel before make teams.");
-    return;
-  }
-
-  const members = makeMemberList(channel);
-  const numOfPlayer = members.length / numOfTeam;
-  randomSort(members);
-
-  let replyText = `yes\n I try to make ${numOfTeam} teams in your voicechannel\n\n`;
-  if(members.length < numOfTeam){
-    replyText += `**Failed!**\nnot enough members in your voice channel to make ${numOfTeam} teams.`;
-  }else if(numOfTeam == 0){
-    replyText += `Are you serious?`; 
-  }else{
-    for(let i = 0; i < numOfTeam; i++){
-      replyText += `**team${i + 1}**\n`;
-      for(let j = 0; j < numOfPlayer; j++){
-        replyText += `[${members[i * numOfPlayer + j].username}]`;
-      }
-      replyText += `\n\n`;
+    if(numOfTeam === undefined){
+        message.reply(
+            `plz input number of team
+            eg) !team 2`);
+        return;
     }
-  } 
 
-  message.reply(replyText)
-    .then(message => console.log("team maked"))
-    .catch(console.error);
+    const channel = findVoicechannel(message);
+
+    if(channel === undefined){
+        message.reply('plz join voicechannel before make teams.');
+        return;
+    }
+
+    const members = makeMemberList(channel);
+    const numOfPlayer = members.length / numOfTeam;
+    randomSort(members);
+
+    let replyText = `yes\n I try to make ${numOfTeam} teams in your voicechannel\n\n`;
+    if(members.length < numOfTeam){
+        replyText += `**Failed!**\nnot enough members in your voice channel to make ${numOfTeam} teams.`;
+    }else if(numOfTeam == 0){
+        replyText += 'Are you serious?'; 
+    }else{
+        for(let i = 0; i < numOfTeam; i++){
+            replyText += `**team${i + 1}**\n`;
+            for(let j = 0; j < numOfPlayer; j++){
+                replyText += `[${members[i * numOfPlayer + j].username}]`;
+            }
+            replyText += '\n\n';
+        }
+    } 
+
+    message.reply(replyText)
+        .then(() => console.log('team maked'))
+        .catch(console.error);
 }
-commandMap.set("!team",teamSuffle);
+commandMap.set('!team',teamSuffle);
 
 function callCommand(message){
-  let called = false;
-  for(commandName of commandMap){
-    if(commandName[0] === message.content.split(" ")[0]){
-      commandName[1](message);
-      called = true;
+    let called = false;
+    for(let commandName of commandMap){
+        if(commandName[0] === message.content.split(' ')[0]){
+            commandName[1](message);
+            called = true;
+        }
     }
-  }
-  if(!called){
-    message.reply("sry I dont have any idea.")
-      .then(message => console.log("undefined function was called."))
-      .catch(console.error);
-  }else{
-    console.log("function called");
-  }
+    if(!called){
+        message.reply('sry I dont have any idea.')
+            .then(() => console.log('undefined function was called.'))
+            .catch(console.error);
+    }else{
+        console.log('function called');
+    }
 }
 
 client.on('ready', () => {
     console.log('ready...');
-    });
+});
 
 //メッセージに対するイベント
-client.on("message", message =>{
+client.on('message', message =>{
     if(message.author.bot){
-    return;
+        return;
     }
 
-    if(message.content.startsWith("!")){
-    callCommand(message);
+    if(message.content.startsWith('!')){
+        callCommand(message);
     }
 
-    if(message.content === "hello"){
-    const reply_text = "hello :)";
-    message.reply(reply_text)
-    .then(message => console.log(`sent message: ${reply_text}`))
-    .catch(console.error);
-    return;
+    if(message.content === 'hello'){
+        const reply_text = 'hello :)';
+        message.reply(reply_text)
+            .then(() => console.log(`sent message: ${reply_text}`))
+            .catch(console.error);
+        return;
     }
-    });
+});
 
 client.login(token);
